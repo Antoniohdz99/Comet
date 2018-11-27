b205c26c8b7020d893e37424b34331a9562dfade
Sunset1234 <emmanuel_burciaga@hotmail.com>
2018-11-26 21:03:39 -0600

    TONY

--- app/src/main/java/com/example/ricky/comet/inicio/Inicio_sesion.java
+++ app/src/main/java/com/example/ricky/comet/inicio/Inicio_sesion.java
@@ -2,15 +2,39 @@ package com.example.ricky.comet.inicio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ricky.comet.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Inicio_sesion extends AppCompatActivity {
+
+
+    EditText txtmail, txtpass;
+    FirebaseAuth myauth;
+    FirebaseUser usuario;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
-        //Aqui hay un comentario
+
+        txtmail=findViewById(R.id.txtmail);
+        txtpass=findViewById(R.id.txtpass);
+
+        myauth=FirebaseAuth.getInstance();
+        usuario=myauth.getCurrentUser();
+
+        if(usuario==null){
+
+        }
+
+
+
+
+
     }
 }
