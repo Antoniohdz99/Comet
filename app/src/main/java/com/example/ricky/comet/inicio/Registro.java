d56d48c4ce228c8be2ec4f85a42227546e26fe73
Antonio <ricky_antonyo@hotmail.com>
2018-11-27 01:37:25 -0600

    Mejoras tecnicas (Parciales)

--- app/src/main/java/com/example/ricky/comet/inicio/Registro.java
+++ app/src/main/java/com/example/ricky/comet/inicio/Registro.java
@@ -1,9 +1,11 @@
 package com.example.ricky.comet.inicio;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +30,9 @@ import java.util.HashMap;
 
 public class Registro extends AppCompatActivity {
 
+    //validacion del usuario ingresado
+    FirebaseUser usuario;
+    FirebaseAuth myauth;
     //Datos del reguistro
     EditText
     nombre,
@@ -66,6 +71,7 @@ public class Registro extends AppCompatActivity {
 
         edad = findViewById(R.id.ag_edad);
 
+        edad.setInputType(InputType.TYPE_NULL);
         edad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -105,35 +111,52 @@ public class Registro extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                if (correo.getText().toString() != "" && contra.getText().toString() != "" && edad.getText().toString() != "" && nombre.getText().toString() != "") {
 
-                    mAuth.createUserWithEmailAndPassword(correo.getText().toString(), contra.getText().toString())
-                            .addOnCompleteListener(Registro.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information
-                                        Log.d("ingresado", "createUserWithEmail:success");
-                                        //Tomando el uid del usuario resien reguistrado
-                                        String User_id = mAuth.getCurrentUser().getUid();
-                                        //ingresando los datos del usuario
-                                        FirebaseDatabase.getInstance().getReference().child("Usuarios").child(User_id).child("nombre").setValue(nombre.getText().toString());
-                                        FirebaseDatabase.getInstance().getReference().child("Usuarios").child(User_id).child("correo").setValue(correo.getText().toString());
-                                        FirebaseDatabase.getInstance().getReference().child("Usuarios").child(User_id).child("edad").setValue(edad.getText().toString());
+                mAuth.createUserWithEmailAndPassword(correo.getText().toString(), contra.getText().toString())
+                        .addOnCompleteListener(Registro.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("ingresado", "createUserWithEmail:success");
+                                    //Tomando el uid del usuario resien reguistrado
+                                    String User_id = mAuth.getCurrentUser().getUid();
+                                    //ingresando los datos del usuario
+                                    FirebaseDatabase.getInstance().getReference().child("Usuarios").child(User_id).child("nombre").setValue(nombre.getText().toString());
+                                    FirebaseDatabase.getInstance().getReference().child("Usuarios").child(User_id).child("correo").setValue(correo.getText().toString());
+                                    FirebaseDatabase.getInstance().getReference().child("Usuarios").child(User_id).child("edad").setValue(edad.getText().toString());
 
 
-                                    } else {
-                                        // If sign in fails, display a message to the user.
-                                        //si el usuario no se pudo crear susedeta lo siguiete
-                                        Log.w("Error", "createUserWithEmail:failure", task.getException());
-                                        Toast.makeText(getApplicationContext(), "fallo al autentificar.", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    //si el usuario no se pudo crear susedeta lo siguiete
+                                    Log.w("Error", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(getApplicationContext(), "fallo al autentificar.", Toast.LENGTH_SHORT).show();
 
-                                    }
-
-                                    // ...
                                 }
-                            });
+
+                                // ...
+                            }
+                        });
+            }else{
+                    Toast.makeText(getApplicationContext(), "LLene todos los datos por favor", Toast.LENGTH_SHORT).show();
+
+                }
 
             }
         });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().signOut();
+        usuario=myauth.getInstance().getCurrentUser();
+
+        //SI EL USUARIO ESTÁ LOGEADO, LO REDIRIJIMOS
+        if(usuario!=null){
+            Intent ir = new Intent(getApplicationContext(),Principal.class);
+            startActivity(ir);
+        }
     }
 }
