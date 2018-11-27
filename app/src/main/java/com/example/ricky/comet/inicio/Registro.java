6d932b1f7e1e485830394ed83984f2c1424121ae
Antonio <ricky_antonyo@hotmail.com>
2018-11-27 11:53:58 -0600

    agrege La verdad hise un chingo de cosas y nose que ya hagrege en este paso

--- app/src/main/java/com/example/ricky/comet/inicio/Registro.java
+++ app/src/main/java/com/example/ricky/comet/inicio/Registro.java
@@ -1,7 +1,12 @@
 package com.example.ricky.comet.inicio;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.ricky.comet.R;
@@ -25,6 +31,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.Calendar;
 import java.util.HashMap;
 
@@ -38,8 +45,10 @@ public class Registro extends AppCompatActivity {
     nombre,
     correo,
     contra,
-
     edad;
+    //Imagen del usuario
+    Button Foto;
+    ImageView Foto_ag;
 //Botton del reguistro
     Button reg;
 //Conesión con la base
@@ -57,6 +66,18 @@ public class Registro extends AppCompatActivity {
 
 
 
+    //Tomar Foto Con la camara
+    private static final  String Carpeta_principal ="misImagenesApp/";//Carpeta principal
+    private static final  String Carpeta_imagen ="imagenes";//donde se guarda la foto
+    private static final  String Directorio_imagen =Carpeta_principal+Carpeta_imagen;//Ruta carpeta y directorio
+    private String Path; //Almacenamiento de la ruta de la imagen
+    File FileImage;
+    Bitmap bitmap;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,9 +89,25 @@ public class Registro extends AppCompatActivity {
         nombre = findViewById(R.id.ag_name);
         correo =findViewById(R.id.ag_correo);
         contra = findViewById(R.id.ag_contra);
-
         edad = findViewById(R.id.ag_edad);
+        //Referencia para la foto
+        Foto =findViewById(R.id.ag_foto);
+        Foto_ag =findViewById(R.id.Foto_ag);
 
+
+
+
+        //onclick de la Foto
+        Foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Mostrara un dialog con opciones
+                MostrarDialogOpciones();
+            }
+        });
+
+
+        //onclik de la Fecha
         edad.setInputType(InputType.TYPE_NULL);
         edad.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -149,6 +186,58 @@ public class Registro extends AppCompatActivity {
             }
         });
     }
+
+    private void MostrarDialogOpciones() {
+        //opciones del dialog
+        final CharSequence[] Opciones = {"Tomar Foto","Elegir de la galeria","Cancelar"};
+        //Creacion del dialog
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //titulo del dialog
+        builder.setTitle("Elige una opción");
+        //inteface del dialog
+        builder.setItems(Opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                switch (Opciones[i].toString())
+                {
+                    case "Tomar Foto":{
+                        Toast.makeText(getApplicationContext(),"Funciona",Toast.LENGTH_LONG).show();
+                    }break;
+                    case "Elegir de la galeria":{
+                        Intent  Imagen = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        Imagen.setType("image/");
+                        startActivityForResult(Imagen.createChooser(Imagen,"Selecione"),10);
+
+                    }break;
+                    case "Cancelar":{
+                        dialogInterface.dismiss();
+                    }break;
+                    default:break;
+                }
+            }
+        });
+        builder.show();
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode)
+        {
+            case 10: {
+                try {
+                    Uri miPath = data.getData();
+                    Foto_ag.setImageURI(miPath);
+                }catch (Exception e){}
+
+            }break;
+            case 20:{}break;
+
+        }
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
