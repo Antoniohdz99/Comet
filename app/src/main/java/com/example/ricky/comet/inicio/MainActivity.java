d1cd29fdbc6b2c8379472b42cf49811807e98dd9
Sunset1234 <emmanuel_burciaga@hotmail.com>
2018-12-03 16:44:21 -0600

    Google Login

--- app/src/main/java/com/example/ricky/comet/inicio/MainActivity.java
+++ app/src/main/java/com/example/ricky/comet/inicio/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.ricky.comet.inicio;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.ricky.comet.Login.Auth_user;
 import com.example.ricky.comet.R;
@@ -15,6 +21,9 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,13 +40,30 @@ Button
         setContentView(R.layout.activity_main);
 
         auth_user = new Auth_user();
-        auth_user.check_user(MainActivity.this );
+        auth_user.check_user(MainActivity.this);
         btn_iniciar_S = findViewById(R.id.log);
         btn_registrar = findViewById(R.id.reg);
 
         btn_iniciar_S.setOnClickListener(Click_iniciar_s);
         btn_registrar.setOnClickListener(Click_Registrar);
 
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.ricky.comet",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", "KeyHash:" + Base64.encodeToString(md.digest(),
+                        Base64.DEFAULT));
+                Toast.makeText(getApplicationContext(), Base64.encodeToString(md.digest(),
+                        Base64.DEFAULT), Toast.LENGTH_LONG).show();
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
 
     }
 
